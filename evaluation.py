e16cb07e2a35d0d8d75ea333f6a0658b8e89c2cc
Ali <aaldakhe@andrew.cmu.edu>
2020-12-04 14:16:40 -0500

    started evaluation code

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,3 @@
-
 """
     Compare ptaint to neutaint
 
@@ -8,3 +7,33 @@
         - compute errors
         - compare run time
 """
+import programs
+import ptaint
+
+class Evaluation():
+    """
+    Takes a list of programs to evaluate on
+    """
+    def __init__(self, program_list, dataset_length=1000):
+        self.program_names = program_list
+        self.dataset_length = dataset_length
+
+        self.program_insts = [
+                getattr(programs, p)() for p in self.program_names
+                ]
+        self.input_datasets = [
+                self.construct_dataset(inst) for inst in self.program_insts
+                ]
+
+    def construct_dataset(self, inst):
+        return inst.gen_inputs(self.dataset_length)
+
+
+if __name__ == "__main__":
+    program_list = [
+            'FBD',
+            'TBPD'
+            ]
+    dataset_length = 100
+    ev = Evaluation(program_list, dataset_length=dataset_length)
+
