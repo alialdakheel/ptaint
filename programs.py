501d0c6f786cfeee1a8c9b646f214aca78b9c7a2
Ali <aaldakhe@andrew.cmu.edu>
2020-12-18 22:05:45 -0500

    cleanup

--- programs.py
+++ programs.py
@@ -186,25 +186,3 @@ class JC(program):
                 }
         return [[ref1], [ref2], [ref3], [ref4]]
 
-def first_byte_value_dependent(input):
-    x = input
-    if x[0] == 0.5:
-        return 1
-    return 0
-
-
-
-def key_swap(query_params):
-    # a program json -> json where values may be passed from input to output
-    # into new keys. the key mapping, if any, is not known.
-    try:
-        d = json.loads(query_params.strip())
-        if ("age" in d and isinstance(d["age"], int) and d["age"] > 0) \
-                and ("name" in d and isinstance(d["name"], str) and \
-                len(d["name"]) > 0):
-            return json.dumps({"person": d["name"], "howOld": d["age"], "somethingElse": random.randint(0, 10)})
-    except:
-        pass
-
-    return 'Error'
-
